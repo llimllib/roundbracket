29a6f9603347524ec9865a46ebca7b2ecb099f97
Bill Mill <bill.mill@gmail.com>
2014-03-21 09:57:14 -0400

    add script to download nate silver data from github

---
+++ dl.py
@@ -0,0 +1,14 @@
+import requests
+
+n = 21
+url = "https://raw.githubusercontent.com/fivethirtyeight/data/master/march-madness-predictions/bracket-{}.csv"
+resp = requests.get(url.format(n))
+while resp.status_code == 200:
+    lastgood = resp
+    n += 1
+    print n
+    resp = requests.get(url.format(n))
+
+# file comes with windows newlines... replace with unix
+text = lastgood.text.replace('\r', '\n')
+open("natesilver.csv", 'w').write(text)
