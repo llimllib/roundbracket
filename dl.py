cf9bd3d7fe68033a25d4d668830b9e3ae2863f40
Bill Mill <bill.mill@gmail.com>
2015-03-18 22:02:33 -0400

    fix dl.py

--- dl.py
+++ dl.py
@@ -1,12 +1,11 @@
 import os, glob
 import requests
 
-os.chdir("../fivethirtyeight-data/march-madness-predictions")
-os.system("git pull")
-fname = list(sorted(glob.glob("*.csv")))[-1]
-os.system("cp {} ../../roundbracket/natesilver.csv".format(fname))
-os.chdir("../../roundbracket")
+def sh(cmd):
+    print cmd
+    os.system(cmd)
 
-# file comes with windows newlines... replace with unix
-text = file("natesilver.csv", "r").read().replace('\r', '\n')
-open("natesilver.csv", 'w').write(text)
+os.chdir("../fivethirtyeight-data/march-madness-predictions-2015/mens")
+sh("git pull")
+fname = list(sorted(glob.glob("*.tsv")))[-1]
+sh("cp {} ../../../roundbracket/natesilver.tsv".format(fname))
