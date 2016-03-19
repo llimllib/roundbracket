a946e29b1c638e97448f322c1b94a8291b6b25ff
Bill Mill <bill@billmill.org>
2016-03-18 23:19:26 -0400

    update

--- fullsizelogos/resize.py
+++ fullsizelogos/resize.py
@@ -5,6 +5,7 @@ def sh(cmd):
     print(cmd)
     os.system(cmd)
 
-for logo in glob("*.png") + glob("*.jpg") + glob("*.gif"):
+#for logo in glob("*.png") + glob("*.jpg") + glob("*.gif"):
+for logo in ["Holy Cross.png"]:
     name = logo[:-4]
     sh('convert "{}" -resize 50x50 "../logos/{}.png"'.format(logo, name))
